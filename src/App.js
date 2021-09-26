31a854197f7cc91966d80692e63ccc4c158dd014
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-09-26 16:38:09 -0300

    Added Header

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 
 function App() {
   return (
       <BrowserRouter>
+          <Header/>
           <div>
               <Switch>
                   <Route exact={true} path="/" component={HomePage} />
