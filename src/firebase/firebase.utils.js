6a45777ba58759ca4811949aaa2e47f50c9cc446
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-09-26 21:31:57 -0300

    Added firebase authentication with google and storing it in firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,33 @@ const firebaseConfig = {
     measurementId: "G-W4ZWFLRG0H"
 };
 
+export const createUserProfileDocument = async(userAuth,additionalData) => {
+    if(!userAuth){
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(error){
+            console.log("Error creating user!" + error.message);
+        }
+
+        return userRef;
+    }
+}
+
 
 firebase.initializeApp(firebaseConfig);
 
